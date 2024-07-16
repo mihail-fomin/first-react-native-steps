008df3e36c5cce99babe0ffd8d10f4269e041cf6
laptop <107790477+mihail-fomin@users.noreply.github.com>
2024-07-16 17:13:04 +0500

    Added sign in button

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -4,12 +4,20 @@ import { SafeAreaView } from 'react-native-safe-area-context'
 
 import {images} from '../../constants'
 import FormField from '@/components/FormField'
+import CustomButtom from '@/components/CustomButtom'
+import { Link } from 'expo-router'
 
 const SignIn = () => {
   const [form, setForm] = useState({
     email: '',
     password: '',
   })
+
+  const [isLoading, setIsLoading] = useState(false)
+
+  const handleSubmit = () => {
+
+  }
 
   return (
     <SafeAreaView className='bg-primary h-full'>
@@ -41,6 +49,20 @@ const SignIn = () => {
             handleChange={(e) => setForm({...form, password: e})}
             otherStyles='mt-7'
           />
+
+          <CustomButtom
+            containerStyles='mt-7'
+            title='Sign in'
+            handlePress={handleSubmit}
+            isLoading={isLoading}
+          />
+
+          <View className='justify-center pt-5 flex-row gap-2'>
+            <Text className='text-lg text-gray-100 font-pregular'>
+              Don't have an account?
+            </Text>
+            <Link href='/sign-up' className='text-lg font-psemibold text-secondary'>Sign Up</Link>
+          </View>
         </View>
       </ScrollView>
     </SafeAreaView>
