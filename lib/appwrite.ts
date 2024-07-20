c07c2fb142498c571fb9c8898f0518df401340ee
laptop <107790477+mihail-fomin@users.noreply.github.com>
2024-07-20 17:27:18 +0500

    Added Appwwrite config

---
+++ lib/appwrite.ts
@@ -0,0 +1,10 @@
+
+export const appwriteConfig = {
+  endpoint: 'https://cloud.appwrite.io/v1',
+  platform: 'com.mary.android',
+  projectId: '669ba815003817b98975',
+  databaseId: '669ba95f0005d0cb6b51',
+  userCollectionId: '669ba98f002b5f7a0b03',
+  videoCollectionId: '669ba9ce001674edb540',
+  storageId: '669bac91003c1577cb47',
+}
